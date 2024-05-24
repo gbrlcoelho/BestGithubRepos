d825bc545af06c1c51bfd0698da029dcb3092558
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-24 20:36:18 -0300

    feat(theme): added app router

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+import {Router} from '@routes';
 import {darkTheme, theme} from '@theme';
 
 const queryClient = new QueryClient();
@@ -16,7 +17,7 @@ export const App = () => {
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
         <ThemeProvider theme={deviceTheme === 'dark' ? darkTheme : theme}>
-          <></>
+          <Router />
         </ThemeProvider>
       </SafeAreaProvider>
     </QueryClientProvider>
