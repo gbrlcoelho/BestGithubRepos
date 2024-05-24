32bfa374a4e8fe9c4b63d51e05e8a40b193dfe8b
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-23 21:08:20 -0300

    feat(theme): app dark and light theme

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import {useColorScheme} from 'react-native';
 
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {theme} from '@theme';
+import {darkTheme, theme} from '@theme';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
+  const deviceTheme = useColorScheme();
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={deviceTheme === 'dark' ? darkTheme : theme}>
           <></>
         </ThemeProvider>
       </SafeAreaProvider>
---
+++ src/theme/colors.ts
@@ -0,0 +1,28 @@
+export const palette = {
+  white: '#EEEEEE',
+  black: '#000000',
+  black70: '#0B0B0B',
+  lightBlue: '#69ACEB',
+  white70: '#FFFFFC',
+  gray: '#BFBDB6',
+  lightGray: '#A7A7A7',
+};
+
+export const colors = {
+  lightTheme: {
+    accentText: palette.lightBlue,
+    background: palette.white,
+    text: palette.black,
+    textContrast: palette.white,
+    secondary: palette.gray,
+    lightSecondary: palette.lightGray,
+  },
+  darkTheme: {
+    accentText: palette.lightBlue,
+    background: palette.black,
+    text: palette.white,
+    textContrast: palette.black,
+    secondary: palette.gray,
+    lightSecondary: palette.lightGray,
+  },
+};
