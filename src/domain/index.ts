b9a3d971a69d3cb199cbb2a50e445f7305a9037a
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-25 00:20:00 -0300

    feat(domain): export pull request service and interface

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,2 +1,4 @@
+export {pullRequestService} from './PullRequest/pullRequestService';
+export type {PullRequest} from './PullRequest/pullRequestTypes';
 export {repositoryService} from './Repository/repositoryService';
 export type {Repository} from './Repository/repositoryTypes';
