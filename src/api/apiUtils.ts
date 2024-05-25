90e3a0f68810ed5ea32507a210f2e98a07f33356
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-25 00:19:40 -0300

    feat(api): treat parseLinkHeader error

--- src/api/apiUtils.ts
+++ src/api/apiUtils.ts
@@ -1,13 +1,17 @@
 const parseLinkHeader = (linkHeader: string): Record<string, string> => {
-  const links = linkHeader.split(', ');
-  const parsedLinks: Record<string, string> = {};
-  links.forEach(link => {
-    const [url, rel] = link.split('; ');
-    const parsedUrl = url.slice(1, -1);
-    const parsedRel = rel.split('=')[1].slice(1, -1);
-    parsedLinks[parsedRel] = parsedUrl;
-  });
-  return parsedLinks;
+  try {
+    const links = linkHeader.split(', ');
+    const parsedLinks: Record<string, string> = {};
+    links.forEach(link => {
+      const [url, rel] = link.split('; ');
+      const parsedUrl = url.slice(1, -1);
+      const parsedRel = rel.split('=')[1].slice(1, -1);
+      parsedLinks[parsedRel] = parsedUrl;
+    });
+    return parsedLinks;
+  } catch (error) {
+    return {};
+  }
 };
 
 export const apiUtils = {
