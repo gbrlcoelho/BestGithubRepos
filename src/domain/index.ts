aae3a32f806d53189b436d4ea696c69136a57bfb
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-25 02:54:49 -0300

    feat(domain): export repository api interface

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,4 +2,4 @@ export {pullRequestService} from './PullRequest/pullRequestService';
 export type {PullRequest} from './PullRequest/pullRequestTypes';
 export {usePullRequestCount} from './PullRequest/useCases/usePullRequestCount';
 export {repositoryService} from './Repository/repositoryService';
-export type {Repository} from './Repository/repositoryTypes';
+export type {Repository, RepositoryAPI} from './Repository/repositoryTypes';
