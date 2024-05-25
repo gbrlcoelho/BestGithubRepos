57c321374b7309ebae7d5ff5fccecb4da407c994
gbrlcoelho <coelhogabriel67@gmail.com>
2024-05-25 00:33:33 -0300

    feat(domain): get pull request count use case

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,5 @@
 export {pullRequestService} from './PullRequest/pullRequestService';
 export type {PullRequest} from './PullRequest/pullRequestTypes';
+export {usePullRequestCount} from './PullRequest/useCases/usePullRequestCount';
 export {repositoryService} from './Repository/repositoryService';
 export type {Repository} from './Repository/repositoryTypes';
